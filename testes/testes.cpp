aef406bb849506d19345cff55e9d4f345d477afe
hussyvel <hussyribeiro@hotmail.com>
2020-08-18 13:18:26 -0300

    atualizando programas C++

--- testes/testes.cpp
+++ testes/testes.cpp
@@ -1,16 +1,36 @@
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
-int main(){
-    int a, b;
-    int result;
 
-    a = 5;
-    b = 2;
-    a = a + 1;
-    result = a -b;
+int main (){
+    cout << "***************************" << endl;//A mensagem impressa é a que está entre aspas
+    cout << "***Seja Bem vindo *********" << endl;
+    cout << "***************************" << endl;
 
-    cout << result <<endl;
+    const int NUMERO_SECRETO = 42;
+      
+    bool nao_acertou = true;
+    
+    while(nao_acertou) {
+        int chute;
+        cout << "Qual o seu chute? ";
+        cin >> chute;
+        cout << "O valor de seu chute é " << chute << endl;
+        bool acertou = NUMERO_SECRETO == chute;
+        bool maior = NUMERO_SECRETO > chute;
 
-    return 0;
-} // namespace std;
+
+
+        if (acertou) {
+            cout<< "Parabens, você acertou o numero secreto " <<endl;
+            nao_acertou = false;
+        
+        }else if (maior) {
+            cout << "Desculpe, o numero_secreto é maior " << maior << endl;
+        } else {
+            cout << "Desculpe o numero_secreto é menor que " << endl;
+        }
+    }
+    cout << "Fim do jogo" << endl;
+}
---
+++ testes2/testes/prog1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+int a = 12;
+
+cout << "olá, o valor de a é " << a << endl;
+return 0;
+}
